cde6d069c5f680ac21edbb0926ba918f9bcdbe75
Gabriel Zurita <gabezurita00@gmail.com>
2021-04-16 15:12:28 -0400

    feat: formulario de registro de item de gasto creado

---
+++ src/views/RegItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="register">
+        <div class ="register_container">
+            <RegistroItem class="register_form"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import RegistroItem from "@/components/RegistroItem.vue";
+
+export default {
+    name: "RegistroItemGasto",
+    components:{
+        RegistroItem,
+    },
+}
+
+</script>
+
+<style scoped>
+
+</style>
