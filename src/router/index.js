02ed8a4a5c5a0568163d20459175571a793e9b11
Bri <torricobritany2@gmail.com>
2021-04-23 18:14:22 -0400

    base login

--- src/router/index.js
+++ src/router/index.js
@@ -4,35 +4,44 @@ import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: "/registro_item",
-    name: "RegistroItemGasto",
-    component: () => import("@/views/RegItem.vue"),
-  },
-  {
-    path: "/registro_unidad",
-    name: "RegistroUnidadGasto",
-    component: () => import("@/views/RegUnidad.vue"),
-  },
-  {
-      path: "/register",
-      name: "RegisterUserPage",
-      component: () => import ("../views/RegisterUserPage.vue"),
-  },
+const routes = [{
+        path: "/",
+        name: "Home",
+        component: Home,
+    },
+    {
+        path: "/about",
+        name: "About",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+    {
+        path: "/registro_item",
+        name: "RegistroItemGasto",
+        component: () =>
+            import ("@/views/RegItem.vue"),
+    },
+    {
+        path: "/registro_unidad",
+        name: "RegistroUnidadGasto",
+        component: () =>
+            import ("@/views/RegUnidad.vue"),
+    },
+    {
+        path: "/register",
+        name: "RegisterUserPage",
+        component: () =>
+            import ("../views/RegisterUserPage.vue"),
+    },
+    {
+        path: "/login",
+        name: "LoginPage",
+        component: () =>
+            import ("../views/LoginPage.vue"),
+    },
 ];
 
 const router = new VueRouter({
