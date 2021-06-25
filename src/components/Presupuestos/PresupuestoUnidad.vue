b82e470bc6b720ea61532b59c47a95070371ad8a
gbrlzrt <gabezurita00@gmail.com>
2021-06-25 18:09:10 -0400

    feat: bunch of loading screens

--- src/components/Presupuestos/PresupuestoUnidad.vue
+++ src/components/Presupuestos/PresupuestoUnidad.vue
@@ -68,7 +68,12 @@
           </div>
         </div>
       </div>
-
+      <div v-if="loading">
+      <div class="loading-info">
+          <div class="clock-loader"></div>
+      </div>
+    </div>
+    <div v-else>
       <div
         v-if="this.listaUnidadesDeGasto.length > 0"
         class="form__unidadesGasto "
@@ -137,6 +142,7 @@
           No existen unidades de gasto en este departamento.
         </p>
       </div>
+    </div>
     </form>
 
     <alert-2
@@ -206,6 +212,7 @@ export default {
   },
   data() {
     return {
+      loading: false,
       presupuesto: {
         facultad: "Seleccione una opcion",
         departamento: "Seleccione una opcion",
@@ -356,10 +363,10 @@ export default {
         this.tope = topePresupuesto;
       } catch (error) {
         this.alert("warning", "Algo salio mal");
-        console.log(error);
       }
     },
     async getObtenerUnidadesDeGasto() {
+      this.loading=!this.loading
       try {
         //verificar si mi depa anterior es igual al original si es no cambio
         if (!this.cambioDepa) {
@@ -397,8 +404,9 @@ export default {
           }
         }
       } catch (error) {
-        console.log(error);
+        this.alert("warning", error);
       }
+      this.loading=!this.loading
     },
     async cambiaFacultad() {
       this.cambioFacu = false;
@@ -465,7 +473,6 @@ export default {
         this.cambioDepa = false;
         this.presupuesto.facultad = this.facultadAnterior;
         this.presupuesto.departamento = this.depaAnterior;
-        console.log("puso cancelar");
       }
     },
     //FACULTAD
@@ -487,7 +494,6 @@ export default {
         this.cambioDepa = false;
         this.presupuesto.facultad = this.facultadAnterior;
         this.presupuesto.departamento = this.depaAnterior;
-        console.log("puso cancelar en facultad");
       }
     },
     validador(value) {
@@ -551,6 +557,7 @@ export default {
       }
     },
     async submitForm() {
+      this.loading=!this.loading
       try {
         let desabilito = await this.verificarTope();
         if (!this.$v.presupuesto.$invalid) {
@@ -579,14 +586,15 @@ export default {
           }
         }
       } catch (error) {
-        console.log(error);
+        this.alert("warning", error);
       }
+      this.loading=!this.loading
     },
   },
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .contenedor__pres {
   background-color: #f7f6f6;
   padding: 40px 60px 40px 60px;
@@ -600,6 +608,58 @@ export default {
   font-weight: 600;
   font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
     Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+.loading-info{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 50vh;
+  margin: 0;
+}
+.clock-loader {
+  --clock-color: #000000;
+  --clock-width: 4rem;
+  --clock-radius: calc(var(--clock-width) / 2);
+  --clock-minute-length: calc(var(--clock-width) * 0.4);
+  --clock-hour-length: calc(var(--clock-width) * 0.2);
+  --clock-thickness: 0.2rem;
+  
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: var(--clock-width);
+  height: var(--clock-width);
+  border: 3px solid var(--clock-color);
+  border-radius: 50%;
+
+  &::before,
+  &::after {
+    position: absolute;
+    content: "";
+    top: calc(var(--clock-radius) * 0.25);
+    width: var(--clock-thickness);
+    background: var(--clock-color);
+    border-radius: 10px;
+    transform-origin: center calc(100% - calc(var(--clock-thickness) / 2));
+    animation: spin infinite linear;
+  }
+
+  &::before {
+    height: var(--clock-minute-length);
+    animation-duration: 2s;
+  }
+
+  &::after {
+    top: calc(var(--clock-radius) * 0.25 + var(--clock-hour-length));
+    height: var(--clock-hour-length);
+    animation-duration: 15s;
+  }
+}
+@keyframes spin {
+  to {
+    transform: rotate(1turn);
+  }
 }
 .form_desc {
   text-align: left;
