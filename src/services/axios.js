df95c5fbd1f677d5d69e36bfa8eeed96825e38e7
gbrlzrt <gabezurita00@gmail.com>
2021-07-15 13:38:46 -0400

    feat: router for production

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:4000/",
-//    baseURL: "https://api-cotizaciones.herokuapp.com/",
+//    baseURL: "http://localhost:4000/",
+    baseURL: "https://api-cotizaciones.herokuapp.com/",
     headers: { "Content-Type": "application/json" },
 });
 
